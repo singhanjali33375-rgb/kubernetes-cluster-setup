966b1e79df928ee57d76bfb3ff7552e121fe8e26
Anjali Singh <singhanjali33375@gmail.com>
2026-02-06 14:44:32 +0530

    Create install-kubernetes.sh

---
+++ scripts/scripts/install-kubernetes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
